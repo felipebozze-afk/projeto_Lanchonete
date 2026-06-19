742950a7a541d1a1187407e8675308bd51efe0ef
felipe <felipe.bozze@aluno.senai.br>
2026-06-19 17:03:47 -0300

    first commit

---
+++ projeto-lanchonete/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./componentes/Header";
+import Login from "./componentes/Login";
+
+
+function App(){
+  return(
+    <>
+      <Header />
+      <Login  />
+    </>
+
+  )
+}
+export default App
---
+++ projeto-lanchonete/src/componentes/Login.jsx
@@ -0,0 +1,12 @@
+function Login(){
+
+    return(
+        <>
+        <h2>Login</h2>
+        
+        </>
+    )
+    
+}
+
+export default Login
