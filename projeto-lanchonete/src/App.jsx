0995ea0514256b44025c8348f8349798554e8c08
unknown <felipe.bozze@aluno.senai.br>
2026-06-23 16:08:23 -0300

    parte 2

--- projeto-lanchonete/src/App.jsx
+++ projeto-lanchonete/src/App.jsx
@@ -5,7 +5,9 @@ import Login from "./componentes/Login";
 function App(){
   return(
     <>
-      <Header />
+      <Header titulo = "Lanchonete Dogão e  Burgão"
+       subtitulo="O melhor do Planeta" />
+       
       <Login  />
     </>
 
